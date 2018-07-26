789d2f71d0e29208fc6c94f0b9ee0672a3c0f95c
T145 <T145@protonmail.com>
2018-07-26 18:17:19 -0400

    Added back the user problems

--- src/Graphs.java
+++ src/Graphs.java
@@ -6,7 +6,10 @@ public enum Graphs {
     FLAT_LINE(new double[][]{{1, 1}, {2, 1}, {3, 1}, {4, 1}, {5, 1}, {6, 1}, {7, 1}, {8, 1}}),
     SQUARE(new double[][]{{1, 1}, {5, 5}, {1, 5}, {5, 1}}),
     SQUARE_WITH_CENTER(new double[][]{{1, 1}, {5, 5}, {1, 5}, {5, 1}, {3, 3}}),
-    RHOMBUS(new double[][]{{2, 2}, {3, 5}, {4, 3}, {5, 6}});
+    RHOMBUS(new double[][]{{2, 2}, {3, 5}, {4, 3}, {5, 6}}),
+    // user-created problems
+    OSCAR_DILEMA(new double[][]{{0, 0}, {1, 1000}, {2, 0}, {3, 1000}}),
+    GREYBEARD(new double[][]{{0, 0}, {1, 99}, {2, 98}, {3, 3}, {4, 4}, {5, 95}});
 
     private final double[][] graph;
 
